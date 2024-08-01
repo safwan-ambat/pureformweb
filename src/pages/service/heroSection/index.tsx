e0ef38645cc56e0c7bb090c0dbe9b79e534cbf18
Safwan Ambat <safwanambat@gmail.com>
2024-08-01 18:07:30 +0530

    test

--- src/pages/service/heroSection/index.tsx
+++ src/pages/service/heroSection/index.tsx
@@ -4,7 +4,7 @@ const HeroSection: React.FC = () => {
   return (
     <section className="text-center px-4 sm:px-[16px] md:px-12  xl:px-[356px] mt-8 sm:mt-12 md:mt-[56px]">
       <h1 className=" font-bold my-0 px-2 sm:px-4 md:px-8 lg:px-[100px]">
-        Design Partner for fast shipping teams
+        Design 
       </h1>
       <p className="text-base sm:text-[16px] md:text-[18px] text-black mt-4 sm:mt-[32px] md:mt-[32px] max-w-3xl mx-auto">
         We work with founders to build their Brand, Product, website, and Pitch Deck using a modular approach. 
