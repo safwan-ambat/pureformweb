7d9b1c55512e5f497c4bd9399efee2eeaeb5b3aa
Safwan Ambat <safwanambat@gmail.com>
2024-08-02 21:31:10 +0530

    mweb fixes

--- src/pages/service/navbar/index.tsx
+++ src/pages/service/navbar/index.tsx
@@ -3,7 +3,7 @@ import Logo from "@/assets/logo.webp";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full flex justify-center mt-[28px] bg-white w-[98px] md:w-[168px] mx-auto  ">
+    <nav className="w-full flex justify-center mt-[28px] bg-white w-[100px] md:w-[168px] mx-auto  ">
       <div ><img src={Logo} alt="Logo" /></div>
     </nav>
   );
