70052c5e3c55758b44a7805bd8db2660e1101329
Safwan Ambat <safwanambat@gmail.com>
2024-08-24 20:35:07 +0530

    work section

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -19,51 +19,53 @@ import web03 from '@/assets/work/w3Web.webp';
 
 const WorksPage: React.FC = () => {
   return (
-    <div className="w-full ">
-      <header className="text-center mt-8 sm:mt-12 lg:mt-[56px]  mb-[72px] sm:mb-[80px]">
-        <h1 className=" font-bold my-0 ">
-          Projects shipped for<br/> <span className='font-georgia italic'>startups to unicorns</span>
-        </h1>
-        <p className="text-base sm:text-[16px] lg:text-[18px] text-black mt-4 sm:mt-6 lg:mt-[32px] max-w-3xl mx-auto  px-[20px] md:px-[60px] ">
-          We are your design partner that's always available
-          as you grow or downsize. You can pause or cancel anytime,
-          just need a quick fix? Deck for a YC call? Come back unpause and reuse your remaining days.
-        </p>
-      </header>
-      
-      <div className="px-4 sm:px-8 md:px-12 lg:px-[60px]">
-        <Template 
-          title='Superdev'
-          about='No-code DevOps automation Platform'
-          mainImage={{ src: `${mainImg}`, alt: "Main image" }}
-          tab1={{ src: `${mob1}`, alt: "Image 1" }}
-          tab2={{ src: `${mob2}`, alt: "Image 2" }}
-          webImg={{ src: `${web}`, alt: "Image 3" }}
-        />
-        <Template 
-          title='Mywasiyat'
-          about='Enabling every Indian to create a legally valid will'
-          mainImage={{ src: `${mainImg01}`, alt: "Main image" }}
-          mob1={{ src: `${mob101}`, alt: "Image 1" }}
-          mob2={{ src: `${mob201}`, alt: "Image 2" }}
-          webImg={{ src: `${web01}`, alt: "Image 3" }}
-        />
-        <Template 
-          title='Unbox'
-          about='Social space for home building'
-          mainImage={{ src: `${mainImg02}`, alt: "Main image" }}
-          mob1={{ src: `${mob102}`, alt: "Image 1" }}
-          mob2={{ src: `${mob202}`, alt: "Image 2" }}
-          webImg={{ src: `${web02}`, alt: "Image 3" }}
-        />
-        <Template 
-          title='Whatsay'
-          about='SasS tool enabling conversational survey on Whatsapp, Telegram'
-          mainImage={{ src: `${mainImg03}`, alt: "Main image" }}
-          mob1={{ src: `${mob103}`, alt: "Image 1" }}
-          mob2={{ src: `${mob203}`, alt: "Image 2" }}
-          webImg={{ src: `${web03}`, alt: "Image 3" }}
-        />
+    <div className="flex justify-center w-full">
+      <div className="w-full max-w-7xl px-4 sm:px-8 md:px-12 lg:px-[60px]">
+        <header className="text-center mt-8 sm:mt-12 lg:mt-[56px] mb-[72px] sm:mb-[80px]">
+          <h1 className="font-bold my-0">
+            Projects shipped for<br/> <span className='font-georgia italic'>startups to unicorns</span>
+          </h1>
+          <p className="text-base sm:text-[16px] lg:text-[18px] text-black mt-4 sm:mt-6 lg:mt-[32px] max-w-3xl mx-auto px-[20px] md:px-[60px]">
+            We are your design partner that's always available
+            as you grow or downsize. You can pause or cancel anytime,
+            just need a quick fix? Deck for a YC call? Come back unpause and reuse your remaining days.
+          </p>
+        </header>
+        
+        <div>
+          <Template
+            title='Superdev'
+            about='No-code DevOps automation Platform'
+            mainImage={{ src: `${mainImg}`, alt: "Main image" }}
+            tab1={{ src: `${mob1}`, alt: "Image 1" }}
+            tab2={{ src: `${mob2}`, alt: "Image 2" }}
+            webImg={{ src: `${web}`, alt: "Image 3" }}
+          />
+          <Template
+            title='Mywasiyat'
+            about='Enabling every Indian to create a legally valid will'
+            mainImage={{ src: `${mainImg01}`, alt: "Main image" }}
+            mob1={{ src: `${mob101}`, alt: "Image 1" }}
+            mob2={{ src: `${mob201}`, alt: "Image 2" }}
+            webImg={{ src: `${web01}`, alt: "Image 3" }}
+          />
+          <Template
+            title='Unbox'
+            about='Social space for home building'
+            mainImage={{ src: `${mainImg02}`, alt: "Main image" }}
+            mob1={{ src: `${mob102}`, alt: "Image 1" }}
+            mob2={{ src: `${mob202}`, alt: "Image 2" }}
+            webImg={{ src: `${web02}`, alt: "Image 3" }}
+          />
+          <Template
+            title='Whatsay'
+            about='SasS tool enabling conversational survey on Whatsapp, Telegram'
+            mainImage={{ src: `${mainImg03}`, alt: "Main image" }}
+            mob1={{ src: `${mob103}`, alt: "Image 1" }}
+            mob2={{ src: `${mob203}`, alt: "Image 2" }}
+            webImg={{ src: `${web03}`, alt: "Image 3" }}
+          />
+        </div>
       </div>
     </div>
   );
